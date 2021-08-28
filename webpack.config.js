23e1134348961506f766afe6d2186164cd59e014
Lina Shin <linashin@TLEs-MacBook-Pro.local>
2021-08-27 18:46:58 -0700

    updated boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/',
+  },
+  mode: process.env.NODE_ENV,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+      publicPath: '/'
+    },
+    port: 8080,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000',
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css?/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
